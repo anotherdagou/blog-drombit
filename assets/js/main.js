cee150181857bc00df861b1627bb99900ffde54e
anotherdago <anotherdagou@gmail.com>
2014-04-04 10:33:14 -0600

    Mantenimiento en archivos

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,8 +9,6 @@
     menuActive.to($(".menu"), .15, {display:"table", height:"100%"})
     .to($(".menu ul"), .5, {opacity:1, display:"table-cell", bottom:"0px", ease:Back.easeOut})
     .to($(".menu #close-menu"), .5, {opacity:1, right:"48px", ease:Back.easeOut});
-    //.to($(".menu ul"), .15, {bottom:"0px"}, "-=.15", ease:SlowMo.ease);
-    /*.staggerFromTo(menuArray, .15, {scale:0, opacity:0}, {display:"block", scale:1, opacity:1}, .15);*/
 
     $('#category').on("click", function(){
     	menuActive.play();
