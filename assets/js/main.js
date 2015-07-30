33b37c0bd2cb714a47d74c89d176589242d3ff67
Diego Armando Catalan <diego@drombit.com>
2015-07-30 11:04:23 -0500

    Añadido por JS la funcionalidad de abrir los enlaces de sitios externos en una nueva pestaña ya que por defecto redcarpet el interprete de markdown utilizado en la configuración de jekyll no permite añadir atributos a los enlaces

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,34 @@
 ;(function ($) {
 
-	//create a TimelineLite instance
-    var menuActive = new TimelineLite({paused:true});
 
     // Fix para los titulos de las secciones en el header
     var category = $("#categoryName").html();
-    if (category == "DISENO") {
+    if (category === "DISENO") {
         $("#categoryName").html("DISEÑO");
-    }else if (category == "OPINION"){
+    }else if (category === "OPINION"){
         $("#categoryName").html("OPINIÓN");
-    };
+    } 
 
     // Menú Móvil
     $('.js-menu-mobile').on('click', function(){
         $('.site-nav__menu .menu').toggleClass('menu--mobile');
     });
- 
+
+    // Generar enlaces en nueva pestaña para dominios externos
+    // 1. Selecciona todos los enlaces del articulo.
+    // 2. Encuentra los enlaces que enlazan a artículos del blog.
+    //    Estos enlaces no serán abiertos en una nueva pestaña.
+    // 3. El método search del punto dos devuelve un -1 con enlaces que no cumplan
+    //    con el parámetro de búsqueda establecido. Estos enlaces serán abiertos en
+    //    una nueva pestaña.
+    var articleLinks = $('.article__content a'); /*[1]*/
+    
+    articleLinks.each(function(){
+        var getDomain = $(this).attr('href').search('blog.drombit.com'); /*[2]*/
+        var link = $(this);
+
+        if (getDomain < 1) { /*[3]*/
+            link.attr('target','_blank');
+        }
+    }); 
 }(jQuery || {}));
